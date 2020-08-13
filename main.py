7694956430244245b72f42cd402b908a34f4ad14
Viraj <vmantri@purdue.edu>
2020-08-12 21:01:15 -0400

    Added a stock addition form

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def main_page():
-    return "Hello World!"
+    return render_template('main_page.html')
 
+@app.route('/add_stock')
+def add_stock_page():
+    return render_template('add_stock.html')
+
+@app.route('/success_or_failure', methods=['POST'])
+def show_success_or_failure_page():
+    stock_name = request.form['stock_name']
+    return render_template('main_page.html')
+    
 
