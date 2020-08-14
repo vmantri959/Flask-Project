d01600bd1adb20cb6a1a782f4cb9cf61772605fb
Viraj <vmantri@purdue.edu>
2020-08-14 13:34:01 -0400

    Added API to get stock information

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import requests
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,6 +13,12 @@ def add_stock_page():
 @app.route('/success_or_failure', methods=['POST'])
 def show_success_or_failure_page():
     stock_name = request.form['stock_name']
+    stock_info = requests.get('https://api.tdameritrade.com/v1/marketdata/' + stock_name + '/quotes?apikey=BECNZHSKXG7K2GNI4FKBG13KBXXQBGJR')
+    if stock_info.text == '{}':
+        print('Please enter a valid stock/ETF')
+    else:
+        print('Valid stock/ETF')
+
     return render_template('main_page.html')
     
 
